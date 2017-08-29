5052fe73c528929a0d93c94de15e44fd448d82df
RanaivosonHerimanitra <air_manitra@yahoo.fr>
2017-08-29 19:47:50 -0400

    fully func react app with a separate express backend

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React, { Component } from 'react';
+import ListContacts from './ListContacts';
 import logo from './logo.svg';
 import './App.css';
-class ContactList extends React.Component {
-    render () {
+const contacts = [
+    {
+      "id": "ryan",
+      "name": "Ryan Florence",
+      "email": "ryan@reacttraining.com",
+      "avatarURL": "http://localhost:5001/ryan.jpg"
+    },
+    {
+      "id": "michael",
+      "name": "Michael Jackson",
+      "email": "michael@reacttraining.com",
+      "avatarURL": "http://localhost:5001/michael.jpg"
+    },
+    {
+      "id": "tyler",
+      "name": "Tyler McGinnis",
+      "email": "tyler@reacttraining.com",
+      "avatarURL": "http://localhost:5001/tyler.jpg"
+    }]
+//class ContactList extends React.Component {
+    /*render () {
         //hard coded contact list
         /*const people = [
             {name:'michael'},
@@ -10,25 +30,22 @@ class ContactList extends React.Component {
             {name:'joseph'}
         ]*/
         //here is another way to define it:
+        /*
         const people = this.props.contacts
         return <ol>
             { people.map(person=> (
                <li key={person.name}> {person.name}</li>
             ))}</ol>
-    }
-}
+            */
+   // }
+//}
+//We pass contacts object array to ListContacts component!
 class App extends Component {
     render() {
         return ( 
         <div className = "App" >
-           <ContactList contacts={[
-               {name:'michael'},
-               {name:'rayan'},
-               {name:'joseph'}]} />
-           <ContactList contacts={[
-               {name:'jean michael'},
-               {name:'peter'},
-               {name:'parker'}]} />
+        
+           <ListContacts contacts={contacts}/>
         </div>
         );
     }
