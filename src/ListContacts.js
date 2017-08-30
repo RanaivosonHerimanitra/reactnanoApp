1619ef23fbe11c48a6ebb602fd2b32f7dbd964d0
RanaivosonHerimanitra <air_manitra@yahoo.fr>
2017-08-29 21:18:46 -0400

    using stateless functionnal component

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,6 +1,6 @@
 import React , {Component} from 'react';
 //map over contact in each contacts array obj
-class ListContacts extends Component {
+/*class ListContacts extends Component {
     render() {
         return <ol className="contact-list">
             
@@ -21,5 +21,29 @@ class ListContacts extends Component {
                   )}
                </ol>
     }
+}**/
+/* using stateless functionnal component to achieve same output**/
+function ListContacts (props) {
+    
+        return (<ol className="contact-list">
+            
+                  {props.contacts.map((contact)=>
+                    <li key={contact.id} className="contact-list-item">
+                        
+                        <div className="contact-avatar" style={{
+                            backgroundImage: `url(${contact.avatarURL})`}}/>
+                            <div className="contact-details">
+                               <p>{contact.name}</p>
+                               <p>{contact.email}</p>
+                            </div>
+                            <button className="contact-remove">
+                                Remove
+                            </button>
+                    </li>
+                   
+                  )}
+               </ol>)
+
+     
 }
 export default ListContacts
